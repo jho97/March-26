0e1cfeafbc05b6d0b05162d57702e87c690abe37
Jonathan Ho <ho.jonathan97@gmail.com>
2022-03-26 14:14:32 -0500

    Got some help and tips on ascii

--- march-26.js
+++ march-26.js
@@ -1,23 +1,38 @@
-const readline = require('readline');
+// const readline = require('readline');
 
-const reader = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// const reader = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
-console.log("Welcome to the password validator tool!")
+// console.log("Welcome to the password validator tool!")
 
-reader.question("Type the password you would like to validate: ", function(input){
-	password = input;
+// reader.question("Type the password you would like to validate: ", function(input){
+// 	password = input;
 
-    if (input.length > 9) {
-        console.log('Password is valid!')
-    } else if (input.length <= 9) {
-        console.log('Password is not valid!')
-    } else
-    reader.close()
+//     if (input.length > 9) {
+//         console.log('Password is valid!')
+//     } else if (input.length <= 9) {
+//         console.log('Password is not valid!')
+//     } else
+//     reader.close()
 
-});
+// });
 
 
-const 
+// const vowels = []
+
+// for (let i = 0; i < 26; i++) {
+//     let keys = 65 + i
+//         vowels.push([`${String.fromCharCode(keys)}`])
+// }
+// console.log(vowels)
+
+
+// Can also be be written without the keys variable
+const vowels = []
+
+for (let i = 0; i < 26; i++) {
+        vowels.push([`${String.fromCharCode(65 +i)}`])
+}
+console.log(vowels)
