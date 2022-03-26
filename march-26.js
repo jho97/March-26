82f3dbf5548c3cf70eb78e0e939bfbdb0548d894
Jonathan Ho <ho.jonathan97@gmail.com>
2022-03-26 14:50:38 -0500

    Finding out new things

--- march-26.js
+++ march-26.js
@@ -1,23 +1,24 @@
-// const readline = require('readline');
+const readline = require('readline');
 
-// const reader = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-// console.log("Welcome to the password validator tool!")
+console.log("Welcome all word fanatics!")
 
-// reader.question("Type the password you would like to validate: ", function(input){
-// 	password = input;
+reader.question("I only want you to type letters here! Anything else will be denied 👎🏼: ", function(input){
+	password = input;
+    const lowerCaseLetters =
+    const upperCaseLetters =
+    if (input.length > 9) {
+        console.log('Password is valid!')
+    } else if (input.length <= 9) {
+        console.log('Password is not valid!')
+    } else
+    reader.close()
 
-//     if (input.length > 9) {
-//         console.log('Password is valid!')
-//     } else if (input.length <= 9) {
-//         console.log('Password is not valid!')
-//     } else
-//     reader.close()
-
-// });
+});
 
 
 // const vowels = []
@@ -30,9 +31,24 @@
 
 
 // Can also be be written without the keys variable
-const vowels = []
+// uppercase letters
+// const upperCaseLetters = []
 
+// for (let i = 0; i < 26; i++) {
+//         upperCaseLetters.push([`${String.fromCharCode(65 + i)}`])
+// }
+// console.log(upperCaseLetters)
+
+// lowercase letters
+// const lowerCaseLetters = []
+
+// for (let i = 0; i < 26; i++) {
+//         lowerCaseLetters.push([`${String.fromCharCode(97 + i)}`])
+// }
+// console.log(lowerCaseLetters)
+
+//keys without brackets
 for (let i = 0; i < 26; i++) {
-        vowels.push([`${String.fromCharCode(65 +i)}`])
+    let keys = 65 + i
+    console.log(String.fromCharCode(keys))
 }
-console.log(vowels)
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+
+for (let i = 0; i < 26; i++) {
+    let keys = 65 + i
+    console.log(String.fromCharCode(keys))
+}
